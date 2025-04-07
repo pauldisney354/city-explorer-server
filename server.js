d22ec400362a300a5eb6cc58fdb746262ebff2c5
Paul Disney <paul.disney354@gmail.com>
2025-04-07 15:55:06 -0700

    update server

--- server.js
+++ server.js
@@ -10,16 +10,16 @@ const PORT = process.env.PORT || 3001;
 // Import routes
 const locationRoute = require('./routes/location');
 const weatherRoute = require('./routes/weather');
-const moviesRoute = require('./routes/movies');
+// const moviesRoute = require('./routes/movies');
 
 // Define API routes
 app.use('/location', locationRoute);
 app.use('/weather', weatherRoute);
-app.use('/movies', moviesRoute);
+// app.use('/movies', moviesRoute);
 
-// Error handling middleware
-const errorHandler = require('./middleware/errorHandler');
-app.use(errorHandler);
+// // Error handling middleware
+// const errorHandler = require('./middleware/errorHandler');
+// app.use(errorHandler);
 
 // Start the server
 app.listen(PORT, () => {
